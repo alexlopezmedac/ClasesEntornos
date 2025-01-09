d18d5935a923a5722a85ad940a7bb28e734fc6c3
alex <alejandro.lopez.sanz@doc.medac.es>
2025-01-09 13:59:39 +0100

    Creados atributos y constructor.

--- src/App.java
+++ src/App.java
@@ -1,17 +1,16 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-
         Coche coche1 = new Coche();
+        coche1.color = "rojo";
+        coche1.anyo = 1998;
+        coche1.modelo = "SF90";
+        coche1.marca = "Ferrari";
 
         System.out.println(coche1);
 
         // coche1 = null;
         // System.out.println(coche1);
 
-        coche1.color = "rojo";
-        coche1.anyo = 1998;
-        coche1.modelo = "SF90";
-        coche1.marca = "Ferrari";
     }
 }
