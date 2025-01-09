d1c4aee89b74194c56356e7c5631955f0ed07f7c
alex <alejandro.lopez.sanz@doc.medac.es>
2025-01-09 14:27:53 +0100

    Creados varios constructores.

--- src/App.java
+++ src/App.java
@@ -1,11 +1,19 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        Coche coche1 = new Coche();
-        coche1.color = "rojo";
-        coche1.anyo = 1998;
-        coche1.modelo = "SF90";
-        coche1.marca = "Ferrari";
+        Coche coche1 = new Coche("rojo",
+                1998,
+                "SF90",
+                "Ferrari");
+
+        Coche coche2 = new Coche(2000,
+                "SF90",
+                "Opel");
+
+        System.out.println(coche2.color);
+
+        Coche coche3 = new Coche(2008,
+                "Mercedes");
 
         System.out.println(coche1);
 
--- src/Coche.java
+++ src/Coche.java
@@ -5,7 +5,30 @@ public class Coche {
     public String modelo;
     public String marca;
 
-    public Coche() {
+    public Coche(String colorCoche,
+            int anyoCoche,
+            String modeloCoche,
+            String marcaCoche) {
+        color = colorCoche;
+        anyo = anyoCoche;
+        modelo = modeloCoche;
+        marca = marcaCoche;
+        System.out.println("Estamos dentro del constructor del coche");
+    }
+
+    public Coche(int anyoCoche, String modeloCoche, String marcaCoche) {
+        color = "desconocido";
+        anyo = anyoCoche;
+        modelo = modeloCoche;
+        marca = marcaCoche;
+        System.out.println("Estamos dentro del constructor del coche");
+    }
+
+    public Coche(int anyoCoche, String marcaCoche) {
+        color = "desconocido";
+        anyo = anyoCoche;
+        modelo = "desconocido";
+        marca = marcaCoche;
         System.out.println("Estamos dentro del constructor del coche");
     }
 
